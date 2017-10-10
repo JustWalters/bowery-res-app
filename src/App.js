74fbbbb2fa6a14193ca89cd9b6152e4ace379864
J. Walters <justin.walters@columbia.edu>
2017-10-10 16:25:21 -0400

    Start building table that takes 2D Array as data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class TableRow extends Component {
+  render() {
+    return (
+      <tr>
+      {this.props.cells.map(cell => <td key={cell}>{cell}</td>)}
+      </tr>
+    );
+  }
+}
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.data = [["STNAME","POP","DATE","state"],
+    ["Alabama","4849377","7","01"],
+    ["Alaska","736732","7","02"],
+    ["Arizona","6731484","7","04"],
+    ["Arkansas","2966369","7","05"],
+    ["California","38802500","7","06"],
+    ["Colorado","5355866","7","08"],
+    ["Connecticut","3596677","7","09"],
+    ["Delaware","935614","7","10"],
+    ["District of Columbia","658893","7","11"]];
+    this.colNames = this.data.splice(0,1);
+  }
+
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+          {this.colNames.map(col => <th key={col}>{col}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {this.data.map(row => <TableRow key={row[0]} cells={row} />)}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Table />
       </div>
     );
   }
