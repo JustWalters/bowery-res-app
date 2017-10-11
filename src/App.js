eb223c2656c7fd09067a1c1d49e7682a97644df5
J. Walters <justin.walters@columbia.edu>
2017-10-11 12:44:46 -0400

    Get displayed data from api call

--- src/App.js
+++ src/App.js
@@ -14,17 +14,10 @@ class TableRow extends Component {
 class Table extends Component {
   constructor(props) {
     super(props);
-    this.data = [["STNAME","POP","DATE","state"],
-    ["Alabama","4849377","7","01"],
-    ["Alaska","736732","7","02"],
-    ["Arizona","6731484","7","04"],
-    ["Arkansas","2966369","7","05"],
-    ["California","38802500","7","06"],
-    ["Colorado","5355866","7","08"],
-    ["Connecticut","3596677","7","09"],
-    ["Delaware","935614","7","10"],
-    ["District of Columbia","658893","7","11"]];
-    this.colNames = this.data.splice(0,1);
+
+    this.state = {
+      colNames: this.props.data.splice(0,1)
+    };
   }
 
   render() {
@@ -32,11 +25,11 @@ class Table extends Component {
       <table>
         <thead>
           <tr>
-          {this.colNames.map(col => <th key={col}>{col}</th>)}
+          {this.state.colNames.map(col => <th key={col}>{col}</th>)}
           </tr>
         </thead>
         <tbody>
-          {this.data.map(row => <TableRow key={row[0]} cells={row} />)}
+          {this.props.data.map(row => <TableRow key={row[0]} cells={row} />)}
         </tbody>
       </table>
     );
@@ -44,10 +37,39 @@ class Table extends Component {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    const apiBase = 'https://api.census.gov/data';
+    const year = '2015';
+    const dataset = 'acs5/profile';
+    const vars = ['DP03_0018E', 'DP03_0019E', 'DP03_0020E'];
+    const geo = `for=${encodeURIComponent('county:061')}&in=${encodeURIComponent('state:36')}`;
+
+    let url = [apiBase, year, dataset].join('/');
+    let getParam = vars.join(',');
+
+    url = `${url}?get=${encodeURIComponent(getParam)}&${geo}`;
+
+    fetch(url)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {}
+    })
+    .then(json => {
+      this.setState({data: json});
+    })
+    .catch(err => {console.error(err);});
+  }
+
   render() {
     return (
       <div className="App">
-        <Table />
+        <Table data={this.state.data} />
       </div>
     );
   }
