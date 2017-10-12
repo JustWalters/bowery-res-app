25771e8715cfa5dd7c0b2c0563a24f184a655f6f
J. Walters <justin.walters@columbia.edu>
2017-10-12 16:37:15 -0400

    Add CensusTable component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class TableRow extends Component {
   render() {
     return (
       <tr>
-      {this.props.cells.map(cell => <td key={cell}>{cell}</td>)}
+      {this.props.cells.map((cell, idx) => <td key={idx}>{cell}</td>)}
       </tr>
     );
   }
@@ -16,8 +16,18 @@ class Table extends Component {
     super(props);
 
     this.state = {
-      colNames: this.props.data.splice(0,1)
+      colNames: [],
+      data: []
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.data && nextProps.data.length >= 2) {
+      this.setState({
+        colNames: nextProps.data.splice(0,1)[0],
+        data: nextProps.data
+      });
+    }
   }
 
   render() {
@@ -25,21 +35,73 @@ class Table extends Component {
       <table>
         <thead>
           <tr>
-          {this.state.colNames.map(col => <th key={col}>{col}</th>)}
+          {this.state.colNames.map((col, idx) => <th key={idx}>{col}</th>)}
           </tr>
         </thead>
         <tbody>
-          {this.props.data.map(row => <TableRow key={row[0]} cells={row} />)}
+          {this.state.data.map((row, idx) => <TableRow key={idx} cells={row} />)}
         </tbody>
       </table>
     );
   }
 }
 
+class CensusTable extends Component {
+  constructor(props){
+    super(props);
+    this.data = [];
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!(nextProps.neighborhoodData[1] && nextProps.countyData[1])) {
+      // Don't do anything unless we have both sets of data
+      return;
+    }
+
+    let neighborhood = nextProps.neighborhood;
+    let county = nextProps.county;
+
+    let countyData = this.calculateValues(nextProps.countyData[1]);
+    let neighborhoodData = this.calculateValues(nextProps.neighborhoodData[1]);
+
+    let row0 = ['', neighborhood, '% Total (neighborhood)', county, '% Total (county)'];
+    let row1 = ['2015 Est. Pop 16+ by Transp. to Work', neighborhoodData.commuters, '', countyData.commuters, ''];
+    let row2 = ['Drove Alone', neighborhoodData.alone, neighborhoodData.alonePercent, countyData.alone, countyData.alonePercent];
+    let row3 = ['Carpooled', neighborhoodData.carpool, neighborhoodData.carpoolPercent, countyData.carpool, countyData.carpoolPercent];
+    let row4 = ['Other', neighborhoodData.other, neighborhoodData.otherPercent, countyData.other, countyData.otherPercent];
+    this.data = [row0, row1, row2, row3, row4];
+  }
+
+  calculateValues(data) {
+    let commuters = Number(data[0]);
+    let alone = Number(data[1]);
+    let carpool = Number(data[2]);
+    let other = commuters - (alone + carpool);
+
+    let alonePercent = alone/commuters;
+    let carpoolPercent = carpool/commuters;
+    let otherPercent = 1 - ((alone + carpool)/commuters);
+
+    return {
+      commuters,
+      alone,
+      carpool,
+      other,
+      alonePercent,
+      carpoolPercent,
+      otherPercent
+    };
+  }
+
+  render() {
+    return <Table data={this.data}/>;
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = { neighborhoodData: [], countyData: [] };
   }
 
   componentDidMount() {
@@ -47,21 +109,38 @@ class App extends Component {
     const year = '2015';
     const dataset = 'acs5/profile';
     const vars = ['DP03_0018E', 'DP03_0019E', 'DP03_0020E'];
-    const geo = `for=${encodeURIComponent('county:061')}&in=${encodeURIComponent('state:36')}`;
+    const geoCounty = `for=county:061&in=state:36`;
+    const geoNeighborhood = `for=zip+code+tabulation+area:10009`;
 
-    let url = [apiBase, year, dataset].join('/');
+    this.neighborhood = 'Slope';
+    this.county = 'Kings';
+
+    let urlCounty, urlNeighborhood, url = [apiBase, year, dataset].join('/');
     let getParam = vars.join(',');
+    url = `${url}?get=${encodeURIComponent(getParam)}`;
 
-    url = `${url}?get=${encodeURIComponent(getParam)}&${geo}`;
+    urlCounty = `${url}&${geoCounty}`;
+    urlNeighborhood = `${url}&${geoNeighborhood}`;
 
-    fetch(url)
+    fetch(urlCounty)
     .then(res => {
       if (res.ok) {
         return res.json();
       } else {}
     })
     .then(json => {
-      this.setState({data: json});
+      this.setState({countyData: json});
+    })
+    .catch(err => {console.error(err);});
+
+    fetch(urlNeighborhood)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {}
+    })
+    .then(json => {
+      this.setState({neighborhoodData: json});
     })
     .catch(err => {console.error(err);});
   }
@@ -69,7 +148,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Table data={this.state.data} />
+        <CensusTable neighborhoodData={this.state.neighborhoodData} countyData={this.state.countyData} neighborhood={this.neighborhood} county={this.county} />
       </div>
     );
   }
